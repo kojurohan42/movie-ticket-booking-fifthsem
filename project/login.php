8dcaa036efb4533810ebd379407a111c06e488b0
Rukstha <rukesh.shrestha545@gmail.com>
2021-12-03 12:41:55 +0545

    Ru

--- project/login.php
+++ project/login.php
@@ -3,31 +3,36 @@
 <html>
 <head>
   <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="login.css">
 
 </head>
 <body>
   <div class="header">
-    <h2>Login</h2>
+    
   </div>
    
-  <form method="post" action="login.php" style=" width: 30%;
-  margin: 50px auto 0px;">
+  <form method="post" action="login.php" >
     <?php include('errors.php'); ?>
-    <div class="input-group">
-      <label>Username</label>
-      <input type="text" name="username" >
-    </div>
-    <div class="input-group">
-      <label>Password</label>
-      <input type="password" name="password">
-    </div>
-    <div class="input-group">
-      <button type="submit" class="btn" name="login_user">Login</button>
-    </div>
-    <p>
+    <div class="loginbox">
+    
+    
+    <img src="img\avatar.png" alt="login" class="avatar">
+    
+
+       <h1>User Login </h1>
+       
+        
+            <p>Username</p>
+            <input type="text" name="username" id= "username" placeholder="Enter Username" required>
+            <p>Password</p>
+            <input type="password" name="password" id="password" placeholder="Enter Password" required>
+            <input type="submit" name="login_user" class="login_user" value="Login">
+            
+
+            <p>
       Not yet a member? <a href="register.php">Sign up</a>
     </p>
+        
   </form>
 </body>
 </html>
